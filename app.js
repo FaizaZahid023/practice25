1533cc8e4eb9a6876e8a8b8f90a82b29451a54fa
FaizaZahid123 <faizazahidali123@gmail.com>
2024-05-21 17:34:41 +0500

    java practice

---
+++ app.js
@@ -0,0 +1,8 @@
+document.write("<h1>Number Guessing Game </h1>");
+var game = +prompt("Guess a number");
+var secNum = 9 ;
+if(game== secNum){
+    alert("you win" )}
+    else{
+        alert("try agin")
+    }
